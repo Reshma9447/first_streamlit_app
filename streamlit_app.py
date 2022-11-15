83e7ef23b1b650c94d0e58b6f94987cfd10666ec
Reshma Sreedharan <110809644+Reshma9447@users.noreply.github.com>
2022-11-15 14:56:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('🥣Breakfast Menu')
 streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled 🥣 🥗 🐔 🥑🍞
+ Free-Range Egg')
